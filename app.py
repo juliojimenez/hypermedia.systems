a4accc5083f7959806214ff8c895467408e7360f
Julio Jimenez <julio@julioj.com>
2023-08-09 16:54:01 -0400

    First example
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ app.py
@@ -0,0 +1,15 @@
+from typing import (
+    Literal
+)
+from flask import (
+    Flask
+)
+
+app: Flask = Flask(__name__)
+
+@app.route("/")
+def index() -> Literal["Hello World"]:
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
