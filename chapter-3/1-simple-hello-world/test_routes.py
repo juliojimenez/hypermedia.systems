99bc20393536d47b7343fceda9399a6a82b992ff
Julio Jimenez <julio@julioj.com>
2023-08-27 21:33:52 -0400

    redirects to a non-existing path so a 302 is all we can hope for
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- chapter-3/1-simple-hello-world/test_routes.py
+++ chapter-3/1-simple-hello-world/test_routes.py
@@ -2,6 +2,6 @@ from app import app
 from werkzeug.test import TestResponse
 
 
-def test_request_example():
+def test_index():
     response: TestResponse = app.test_client().get("/")
     assert response.data == b"Hello World!"
